742556f96385dc5a5d8ad88cda2801b799a47aa1
ColoredLemon <44214598+ColoredLemon@users.noreply.github.com>
2022-04-02 16:27:52 -0500

    Update Member.java

--- ICS-499/src/main/java/com/musicBackend/musicBackend/models/Member.java
+++ ICS-499/src/main/java/com/musicBackend/musicBackend/models/Member.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.Collection;
 import java.util.Collections;
 
